ad4616a06d3a4d4a781be94f8a703eb0e20d997b
bigdata1 <tsbslteisrhy@naver.com>
2020-09-18 12:43:27 +0900

    2020/09/18 수정

--- Test/test2.R
+++ Test/test2.R
@@ -38,13 +38,62 @@ df_new <- merge(x = df_new,
 
 df_new <- df_new[,-1]
 df_new <- t(df_new)
-colnames(df_new) <- c('safe', 'operate', 'use', 'access', 'info', 'clean', 'transfer')
+colnames(df_new) <- c('operate', 'use', 'clean', 'info', 'clean', 'transfer', 'safe')
 View(df_new)
 
-#df_new$total <- df_new$safe +
-#                df_new$operate +
-#                df_new$use + 
-#                df_new$access +
-#                df_new$info +
-#                df_new$clean + 
-#                df_new$transfer
+df_new %>% mutate(total=safe+operate+use+access+info+clean+transfer, mean=total/7)
+View(df_new)
+
+
+#독립변수와 종속변수 생성
+x <- df_new$clean
+y <- df_new$use
+
+df_xy <- data.frame(x, y)
+View(df_xy)
+
+#단순선형회귀모델 생성
+result_lm <- lm(formula = y ~ x, data = df_xy)
+result_lm
+
+#회귀식
+simple_linear <- function(x){
+  y <- 0.7393 * x + 0.7789
+  return(y)
+}
+
+#예측
+y1 <- simple_linear(1)
+y2 <- simple_linear(2)
+y3 <- simple_linear(3)
+y4 <- simple_linear(4)
+y5 <- simple_linear(5)
+
+y1
+y2
+y3
+y4
+y5
+
+plot(df_new$use ~ df_new$clean, data = df_new)
+abline(result_lm, col('red'))
+
+
+df_product <- read.csv('../file/product.csv')
+
+#독립변수와 종속변수 생성
+x <- df_product$제품_적절성
+y <- df_product$제품_만족도
+
+df_xy <- data.frame(x, y)
+View(df_xy)
+
+#단순선형회귀모델 생성
+result_lm <- lm(formula = y ~ x, data = df_xy)
+result_lm
+
+#회귀식
+simple_linear <- function(x){
+  y <- 0.7393 * x + 0.7789
+  return(y)
+}
